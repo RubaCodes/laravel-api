d6951a1266d1fbf10f1d2ae2a8a8969a1da232cf
RubaCodes <rubacodes@gmail.com>
2022-07-26 15:06:58 +0200

    first commit

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function categories(){
+        return $this->hasOne('App\Category');
+    }
+
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
+}
